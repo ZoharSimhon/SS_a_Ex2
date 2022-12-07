1da26bf7e6fb66e216df74c4c5789641c6e33f88
ZoharSimhon <118903543+ZoharSimhon@users.noreply.github.com>
2022-12-07 23:20:46 +0200

    final

--- main.c
+++ main.c
@@ -6,28 +6,27 @@ int main(){
     char choise_function;
     int i,j;
 
-    do{
+    do
+    {
         scanf("%c", &choise_function);
-        switc(choise_function) {
-            case 'A':
-                get_matrix_value();
-                break;
-            case 'B':
+        if(choise_function == 'A'){
+                get_matrix_values();
+            }
+            else if(choise_function == 'B'){
                 scanf("%d %d", &i, &j);  
                 int answer = is_there_path(i, j);
                 if(answer == 0){
                     printf("False\n");
                 }
                 else
-                printf("True\n");
-                break;  
-            case 'C':
+                printf("True\n");  
+            }
+            else if (choise_function == 'C'){
                 scanf("%d%d \n", &i, &j);            
                 int answer = shortest_path(i,j);
-                printf("%d\n", answer);  
-                break;   
-        }
-    }while(choise_function != 'D');
+                printf("%d\n", answer);
+            }    
+    } while(choise_function != 'D');
 
     return 0;
 }
